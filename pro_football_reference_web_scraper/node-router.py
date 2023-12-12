6306c4a216d96369bde76d4fd0d8277b20bfa45f
Ryan Hayward <rchayward@Ryans-MacBook-Pro-2.local>
2023-12-12 16:58:12 -0600

    added node router for player game log

---
+++ pro_football_reference_web_scraper/node-router.py
@@ -0,0 +1,16 @@
+import sys
+import player_game_log as p
+
+'''
+The second argument (sys.argv[1]) is designed to contain the indicator for which method we would like to call
+KEY BELOW
+"gameLog" == Get Player Game Log
+'''
+
+if sys.argv[1] == "gameLog":
+    name = sys.argv[2]
+    position = sys.argv[3]
+    year = int(sys.argv[4])  #MUST typecast year
+    print(p.get_player_game_log(name, position, year).to_html)
+    
+sys.stdout.flush()
--- pro_football_reference_web_scraper/test-spawn.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-new_val = int(sys.argv[1]) + int(sys.argv[2])
-
-print(new_val)
-
-sys.stdout.flush()
