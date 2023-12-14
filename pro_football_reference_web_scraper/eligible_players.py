fb9e2fdbd4d6c504e6db7e72bc80ca48738c459a
Ryan Hayward <rchayward@Ryans-MacBook-Pro-2.local>
2023-12-14 14:31:23 -0600

    finished first version of eligible_players.py

--- pro_football_reference_web_scraper/eligible_players.py
+++ pro_football_reference_web_scraper/eligible_players.py
@@ -2,8 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-VALID_STATS = ['passing', 'scrimmage', 'kicking']
-VALID_POSITIONS = ['QB', 'RB', 'WR', 'TE', 'K']
+VALID_STATS = ['passing', 'scrimmage', 'kicking'] # valid statistics to request for
+VALID_POSITIONS = ['QB', 'RB', 'WR', 'TE', 'K'] # valid positions in databasee
+REQUEST_COUNTER = 0 # count requests
 
 '''
 A function that takes statistic type and year as arguments and returns a pandas dataframe of player fantasy stats
@@ -28,17 +29,12 @@ def get_eligible_players(stat_type: str, season: int) -> pd.DataFrame:
     # find appropriate url for request
     request_url = 'https://www.pro-football-reference.com/years/%s/%s.%s' % (str(season), stat_type, 'htm')
     
-    # store response from request
-    response = requests.get(request_url)
-    
-    # get beautiful soup text object and find the table element
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # get BS object from the target URL
+    soup = get_soup(request_url)
+    # find the desired HTML component
     table_rows = soup.find('tbody').find_all('tr')
 
-    # get the list of elements in the table to ignore
-    ignore_list = get_ignore_list(stat_type, table_rows)
-
-    # set the structure for the return data frame
+    # set the base structure for the return data frame
     data = {
         'playerID': [],
         'name': [],
@@ -47,133 +43,64 @@ def get_eligible_players(stat_type: str, season: int) -> pd.DataFrame:
         'season': []
     }
 
-    # add data to the data dictionary
-    for i in range(len(table_rows)):
-        if i not in ignore_list:
-            # calculate unique player ID (FirstLastPositionYear)
-            # Includes caveat for Baker Mayfield 2022 as well as string cleansing for player name
-            name = (table_rows[i].find('td', {'data-stat': 'player'}).text).replace('*', '')
-            name = name.replace('+', '')
-            position = table_rows[i].find('td', {'data-stat': 'pos'}).text
-            if len(position) == 0:
-                position = 'QB'
-            playerID = get_player_id(name, position, season)
+    # determine the eligible positions given the stat type
+    eligible_positions = get_eligible_positions(stat_type)
 
+    # loop through each row in the table
+    for i in range(len(table_rows)):
+        # store position object from the row attribute for position
+        position_object = table_rows[i].find('td', {'data-stat': "pos"})
+
+        # skip over table breaks that have no position attribute at all
+        # store position's content as text
+        if position_object is None:
+            continue
+        else:
+            position = position_object.text
+
+        # if position value is blank, call helper method to populate
+        if len(position) == 0:
+            href = table_rows[i].find('a').get('href')
+            position = get_player_position(href)
+
+        # if the player is not in an eligible position, ignore. Else, add line to the dataframe
+        if position not in eligible_positions:
+            continue
+        else:
+            # get player name and strip irrelevant characters
+            name = (table_rows[i].find('td', {'data-stat': 'player'}).text).replace('*', '').replace('+', '')
+            # use player name, position, and season to get player id
+            playerID = get_player_id(name, position, season)
             #assign values to data dict
             data['playerID'].append(playerID)
             data['name'].append(name)
             data['position'].append(position)
             data['age'].append(int(table_rows[i].find('td', {'data-stat': 'age'}).text))
             data['season'].append(season)
-    
-    '''
-    # adding data to data dictionary
-    for i in range(len(table_rows)):
-        if i not in to_ignore:
-            data['date'].append(table_rows[i].find('td', {'data-stat': 'game_date'}).text)
-            data['week'].append(int(table_rows[i].find('td', {'data-stat': 'week_num'}).text))
-            data['team'].append(table_rows[i].find('td', {'data-stat': 'team'}).text)
-            data['game_location'].append(table_rows[i].find('td', {'data-stat': 'game_location'}).text)
-            data['opp'].append(table_rows[i].find('td', {'data-stat': 'opp'}).text)
-            data['result'].append(table_rows[i].find('td', {'data-stat': 'game_result'}).text.split(' ')[0])
-            data['team_pts'].append(
-                int(table_rows[i].find('td', {'data-stat': 'game_result'}).text.split(' ')[1].split('-')[0])
-            )
-            data['opp_pts'].append(
-                int(table_rows[i].find('td', {'data-stat': 'game_result'}).text.split(' ')[1].split('-')[1])
-            )
-            data['cmp'].append(int(table_rows[i].find('td', {'data-stat': 'pass_cmp'}).text)) if table_rows[i].find(
-                'td', {'data-stat': 'pass_cmp'}
-            ).text != '' else data['cmp'].append(0)
-            data['att'].append(int(table_rows[i].find('td', {'data-stat': 'pass_att'}).text)) if table_rows[i].find(
-                'td', {'data-stat': 'pass_att'}
-            ).text != '' else data['att'].append(0)
-            data['pass_yds'].append(int(table_rows[i].find('td', {'data-stat': 'pass_yds'}).text)) if table_rows[
-                i
-            ].find('td', {'data-stat': 'pass_yds'}).text != '' else data['pass_yds'].append(0)
-            data['pass_td'].append(int(table_rows[i].find('td', {'data-stat': 'pass_td'}).text)) if table_rows[i].find(
-                'td', {'data-stat': 'pass_td'}
-            ).text != '' else data['pass_td'].append(0)
-            data['int'].append(int(table_rows[i].find('td', {'data-stat': 'pass_int'}).text)) if table_rows[i].find(
-                'td', {'data-stat': 'pass_int'}
-            ).text != '' else data['int'].append(0)
-            data['rating'].append(float(table_rows[i].find('td', {'data-stat': 'pass_rating'}).text)) if table_rows[
-                i
-            ].find('td', {'data-stat': 'pass_rating'}).text != '' else data['rating'].append(0)
-            data['sacked'].append(int(table_rows[i].find('td', {'data-stat': 'pass_sacked'}).text)) if table_rows[
-                i
-            ].find('td', {'data-stat': 'pass_sacked'}).text != '' else data['sacked'].append(0)
-            data['rush_att'].append(int(table_rows[i].find('td', {'data-stat': 'rush_att'}).text)) if table_rows[
-                i
-            ].find('td', {'data-stat': 'rush_att'}).text != '' else data['rush_att'].append(0)
-            data['rush_yds'].append(int(table_rows[i].find('td', {'data-stat': 'rush_yds'}).text)) if table_rows[
-                i
-            ].find('td', {'data-stat': 'rush_yds'}).text != '' else data['rush_yds'].append(0)
-            data['rush_td'].append(int(table_rows[i].find('td', {'data-stat': 'rush_td'}).text)) if table_rows[i].find(
-                'td', {'data-stat': 'rush_td'}
-            ).text != '' else data['rush_td'].append(0)
-    '''
-
+    # return data frame
     return pd.DataFrame(data=data)
 
 
-'''
-A helper function that removes non-target positions from consideration
 
-    i.e. for throwing stats, only consider QBs, for scrimmage yards, only consider RB, WR, TE
+'''
+A helper method to make requests to a webpage for HTML and convert the HTML to a Beautiful Soup object.
+A counter has been included to track the # of requests made in a given run
 
     Args:
-        stat_type (str): type of statistic (passing, scrimmage, kicking)
-        table_rows (str): a "stringified" version of the table that appears on the web page (from bs4)
-
+        request_url (str): address of page we would like to scrape
+    
     Returns:
-        ignore_list (list): list of 'rows' from table_rows that we can ignore in our final parse
-
-    Note: we are omitting positions from their secondary stat tpes because the point of this meethod is NOT 
-    to accumulate stats, but to find players that we will search game logs for
+        BeautifulSoup object representing parsed html
 '''
-def get_ignore_list(stat_type: str, table_rows: str) -> list:
-    # store list of indices to ignore here
-    ignore_list = []
+def get_soup(request_url: str) -> BeautifulSoup:
+    # store response from request
+    response = requests.get(request_url)
+    # update the request counter
+    global REQUEST_COUNTER
+    REQUEST_COUNTER += 1
+    # return soup
+    return BeautifulSoup(response.text, 'html.parser')
 
-    # limit list of passers to QBs ONLY
-    if stat_type == 'passing':
-
-        # loop through each row in the table
-        for i in range(len(table_rows)):
-
-            # store position from the row attribute for position
-            position_object = table_rows[i].find('td', {'data-stat': "pos"})
-            
-            # if position object is None, add to the ignore list and restart loop
-            if position_object is None:
-                ignore_list.append(i)
-                continue
-            
-            # else grab text from position object
-            else:
-                position = position_object.text
-
-            # outlier, but if position is empty then convert to QB (observed with 2022 Baker Mayfield)
-            if len(position) == 0:
-                position = 'QB'
-
-            # if the player is not a QB, add the row to the ignore list
-            if position != 'QB':
-                ignore_list.append(i)
-    
-    # limit list of scrimmage stat accumulators to RBs, WRs, and TEs only
-    # @TODO
-    elif stat_type == 'scrimmage':
-        pass
-    
-    # limit list of kickers to Ks only
-    # @TODO
-    elif stat_type == 'kicking':
-        pass
-
-    # return the list of indices to ignore
-    return ignore_list
 
 
 '''
@@ -187,14 +114,70 @@ same player
     
     Returns:
         playerid (str): player-season unique identifier
+
+    @TODO come back and create a better unique ID
 '''
 def get_player_id(name: str, position: str, season: int):
-    # @TODO come back and make a better ID
     return (name + position + str(season)).replace(' ', '')
 
       
+
+'''
+For cases in which player position is not listed in the table, we must inspect the player's page to find
+their position
+
+    Args:
+        href: the player's page url. Can be found in the table itself
+
+    Returns:
+        position: two character player position
+'''
+def get_player_position(href: str) -> str:
+    #get the PFR root url to attach to player name
+    root_url = 'https://www.pro-football-reference.com/'
+    # get BS object from the target URL
+    soup = get_soup(root_url + href)
+    # get player metadata
+    meta = soup.find('div', {'id': 'meta'})
+    # find player position
+    position_strong = meta.find(string="Position")
+    # find parent element of player position and extract text
+    parent = position_strong.find_parent('p').text
+    # split string on "position: " and return the next two characters and strip whitespace (for kickers)
+    return parent.split("Position: ", 1)[1][:2].strip()
+
+
+
+'''
+A helper function to get the eligible positions for a given stat type. Since this script is about determining
+all the players in a given year that accumulated fantasy stats WITHOUT repeating players, we want to restrict
+QBs to passing stats, RBs/WRs/TEs to scrimmage stats, and Ks to kicking stats. We can then use these lists of
+player names alongside player_game_log.py to find all player game logs for a given season and position
+
+    Args:
+        stat_type (str): type of statistic (passing, scrimmage, kicking)
+    
+    Returns:
+        positions (list): list of eligible positions
+'''
+def get_eligible_positions(stat_type: str) -> list:
+    # declare array to store positions
+    positions = []
+    # assign valid positions based on stat type
+    if stat_type == "passing":
+        positions.append('QB')
+    elif stat_type == "scrimmage":
+        positions.extend(('RB', 'WR', 'TE'))
+    elif stat_type == "kicking":
+        positions.append('K')
+    # return eligible positions
+    return positions
+
+
+
 def main():
-    print(get_eligible_players('passing', 2022))
+    print(get_eligible_players('kicking', 2022).to_string())
+    print(REQUEST_COUNTER)
 
 
 
